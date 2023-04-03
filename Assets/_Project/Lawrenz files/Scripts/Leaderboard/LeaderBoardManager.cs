a1bcdccba9e1df2b2c557c253b9eee9300b5842d
Lawrenz Del Rosario <98245945+thegreatwowens@users.noreply.github.com>
2023-04-03 11:55:48 +0800

    Update 4-3-2023
    *added Leaderboard Script

---
+++ Assets/_Project/Lawrenz files/Scripts/Leaderboard/LeaderBoardManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ddr.RockPaperScissor.PlayerManager;
+
+namespace ddr.RockPaperScissor.Leaderboard {
+public class LeaderBoardManager : MonoBehaviour
+{
+    PlayerData playerData;
+    
+    
+   
+   
+}
+
+}
+
